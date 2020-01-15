e453ac9586c7bb9ca4766fbca6054ba13284afbd
salemalem <wymedia@protonmail.com>
2020-01-15 17:11:52 +0600

    Initial commit

---
+++ app/src/main/java/com/example/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.downloadingwebcontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                int data = reader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    response += current;
+                    data = reader.read();
+                }
+
+                return response;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed";
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+        try {
+            result = task.execute("https://www.google.com/").get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.i("response", result);
+    }
+}
